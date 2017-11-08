22a535f6aa6f370260b67cb23b1084c4fa923cda
ryangek <artistkanjai1@gmail.com>
2017-11-08 19:06:47 +0700

    ryangek

--- location.php
+++ location.php
@@ -1,43 +1,5 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	 <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-<style>
-		html, body {
-		background-color: #FFFFFF;
-		color: #000000;
-		font-family: 'Raleway', sans-serif;
-		font-weight: 0;
-		height: 100vh;
-		margin: 0;
-		}
-	</style>
-</head>
-<body>
-	<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#">Car Park Management System</a>
-				</div>
-					<ul class="nav navbar-nav">
-						<li ><a href="home1.php">หน้าหลัก</a></li>
-						<li><a href="user.php">จัดการผู้ใช้งาน</a></li>
-						<li class="active" ><a href="location.php">จัดการแผนที่</a></li>
-						<li><a href="rfid.php">จัดการอาเอฟไอดี</a></li>
-					</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"> <span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-					<li><a href="#"> <span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-				</ul>
-			</div>
-		</nav>
+<?php require_once "layout/header.php";?>
+	<?php require_once "layout/navbar.php"; ?>
 		
 
 				<div class="col-sm-5" >
@@ -57,66 +19,67 @@
 
 	
 
-		<!-- Modal -->
 	<div id="myModal" class="modal fade" role="dialog">
 		<div class="modal-dialog modal-lg">
 
-	 <!-- Modal content-->
 			<div class="modal-content">
 
-			    <div class="modal-header">
-						      
-					<h4 class="modal-title">เพิ่มข้อมูล</h4>
-				</div>
-				 <div class="modal-body">
+	      <div class="modal-header">   
+			     <h4 class="modal-title">เพิ่มข้อมูล</h4>
+		    </div>
+
+		    <div class="modal-body">
+
 					<div class="container">
-  <form action="javascript:void(0)" (submit)="addLocate($event)">
-  <div class="col-md-4">
-    <div class="form-group">
-      <input type="hidden" name="locate_image" #locateImage>
-      <input type="file"
-             accept="image/*"
-             (change)="onChangeImg($event)"
-             style="display: none;"
-             #fileInput
-             name="fileInput">
-      <img (click)="onClickUpload()" src="http://americanconstruction.net/wp-content/uploads/2015/10/upload-empty.png"  class="img-thumbnail" #imgView>
-    </div>
-  </div>
-  <div class="col-md-5">
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <div class="form-inline">
-          <div class="form-group">
-          	
-            <label for="locate_name">ชื่อแผนที่:</label>
-            <input type="text" class="form-control" id="locate_name" placeholder="กรุณาระบุแผนที่" required>
+
+            <form action="javascript:void(0)">
+
+              <div class="col-md-5">
+                <div class="form-group">
+                  <input type="hidden" name="locate_image">
+                  <input type="file"
+                         accept="image/*"
+                         style="display: none;"
+                         name="fileInput">
+                  <img src="http://americanconstruction.net/wp-content/uploads/2015/10/upload-empty.png"  class="img-thumbnail">
+                </div>
+              </div>
+
+              <div class="col-md-7">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <div class="form-inline">
+
+                      <div class="form-group">
+                        <label for="locate_name">ชื่อแผนที่:</label>
+                        <input type="text" class="form-control" id="locate_name" placeholder="กรุณาระบุชื่อแผนที่" required>
+                      </div>
+
+                      <div class="form-group">
+                        <label for="locate_floor">&nbsp;ชั้น:</label>
+                        <input type="number" class="form-control" id="locate_floor" placeholder="กรุณาระบุชั้น" required>
+                      </div>
+
+                      <button type="submit" id="submit" class="btn btn-success">เพิ่ม</button>
+                      <button type="reset" id="reset" class="btn btn-danger">ล้าง</button>
+
+                    </div>
+                    <br>
+                    <div id="alert" class="alert alert-danger">
+                        Please select image !<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </form>   
+
           </div>
-          	
-          <div class="form-group">
-            <label for="locate_floor">&nbsp;ลำดับชั้น:</label>
-            <input type="number" class="form-control" id="locate_floor" placeholder="กรุณาระบุลำดับชั้น" required>
-          </div></br></br>
-          <button type="submit" id="submit" class="btn btn-success">เพิ่มข้อมูล</button>
-          <button type="reset" id="reset" (click)="onClear()" class="btn btn-danger">แก้ไขข้อมูล</button>
-        </div>
-        <br>
-        <div id="alert" *ngIf="error.locate_name||error.locate_floor||error.locate_image" class="alert alert-danger">
-          Please select image !<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-        </div>
+
+    		</div>
+
       </div>
+
     </div>
-			</div>
-
-				<div class="modal-footer">
-						      
-				</div>				
-			</div>
-		</div>
-	</div>
-	
-	</div>
-</form>
-</body>
-</html>
-
+  </div>  
+<?php require_once "layout/footer.php"; ?>
--- user.php
+++ user.php
@@ -1,45 +1,5 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	 <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-<style>
-		html, body {
-		background-color: #FFFFFF;
-		color: #000000;
-		font-family: 'Raleway', sans-serif;
-		font-weight: 0;
-		height: 100vh;
-		margin: 0;
-		}
-	</style>
-</head>
-<body>
-	<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#">Car Park Management System</a>
-				</div>
-					<ul class="nav navbar-nav">
-						<li ><a href="home1.php">หน้าหลัก</a></li>
-						<li class="active"><a href="user.php">จัดการผู้ใช้งาน</a></li>
-						<li><a href="location.php">จัดการแผนที่</a></li>
-						<li><a href="rfid.php">จัดการอาเอฟไอดี</a></li>
-					</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#"> <span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-					<li><a href="#"> <span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-				</ul>
-			</div>
-		</nav>
-		
-			</div>
+<?php require_once "layout/header.php";?>
+	<?php require_once "layout/navbar.php"; ?>
 		
 		<div class="slide-docs-nav">
 				<div class="col-sm-5" >
@@ -117,5 +77,4 @@
 	</div>	
 		</div>	
 
-</body>
-</html>
+<?php require_once "layout/footer.php"; ?>
